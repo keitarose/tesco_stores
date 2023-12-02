1c7a7d2afb2d8c8a702909fe67cf565c6c8a4a57
Keita Rose <keita.rose@kantar.com>
2023-12-02 21:31:09 +0000

    Added file path for the concessions list

--- tesco_store_details/constants.py
+++ tesco_store_details/constants.py
@@ -1,3 +1,4 @@
 START_URL = r"https://www.tesco.com/store-locator/"
 STORE_DATA_FILEPATH = r"./data/tesco_store.json"
 LOCATION_DATA_FILEPATH = r"./data/tesco_location.json"
+CONCESSION_DATA_FILEPATH = r"./data/tesco_concession.txt"
