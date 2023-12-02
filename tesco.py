4c7fad559f36682d9eb9254048e50bda89d8df56
Keita Rose <keita.rose@kantar.com>
2023-12-02 22:06:17 +0000

    Just uncommenting some lines

--- tesco.py
+++ tesco.py
@@ -6,7 +6,8 @@ with Tesco(teardown=True) as bot:
     bot.get_store_regions()
     bot.get_store_details()
     bot.get_concession_details()
-    # bot.write_to_file()
-    bot.location_list()
+    bot.write_to_file()
+    # bot.location_list()
+    time.sleep(5)
 
 # time.sleep(5)
